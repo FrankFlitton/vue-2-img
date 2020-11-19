be4005eec1e3337b7b5d07a6d003f88d73dd16eb
Frank Flitton <fflitton@gmail.com>
2020-11-19 05:27:48 -0500

    Fix Import

--- src/vue2img.js
+++ src/vue2img.js
@@ -1,10 +1,10 @@
 import { convertImg } from './domConverters/convertImg'
-import { convertPDF } from './domConverters/convertImg'
+import { convertPdf } from './domConverters/convertPdf'
 
 const vue2img = () => {
   return {
     image: convertImg,
-    pdf: convertPDF
+    pdf: convertPdf
   }
 }
 
