46a2395ad571dc660d9ba78009bdf0dbd7cefa5a
Frank Flitton <fflitton@gmail.com>
2020-11-19 02:19:41 -0500

    getDate util export

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,11 @@
 import { camelize } from './camelize'
 import { getStyle } from './getStyle'
 import { createOffscreenCanvas } from './createOffscreenCanvas'
+import { getDate } from './getDate'
 
 export {
   camelize,
   getStyle,
-  createOffscreenCanvas
+  createOffscreenCanvas,
+  getDate
 }
