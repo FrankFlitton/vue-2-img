c5735e555752dc2a4ae55e3d1af25d225644b99f
Frank Flitton <fflitton@gmail.com>
2020-11-18 21:30:43 -0500

    Solve CORS issues and imge size issue

---
+++ src/imgTo64.js
@@ -0,0 +1,33 @@
+// Fix for cors bug
+// https://github.com/niklasvh/html2canvas/issues/1544
+
+const imgTo64 = (target, imgList) => {
+  const imgElements = document.querySelectorAll(target + ' img')
+  // Replace all images with Base64 version
+  imgElements.forEach((imageNode) => {
+    // Replace after rendering
+    if (imgList) imgList(imageNode.src)
+
+    // Fix CORS issue
+    imageNode.setAttribute('crossOrigin', 'anonymous')
+
+    // Create canvas
+    const c = createOffscreenCanvas()
+    c.width = imageNode.width
+    c.height = imageNode.height
+    const ctx = c.getContext('2d')
+
+    // Apply css filters
+    ctx.filter = window.getComputedStyle(imageNode).filter
+    ctx.drawImage(imageNode, 0, 0, c.width, c.height)
+
+    imageNode.src = c.toDataURL('image/png')
+  })
+}
+
+function createOffscreenCanvas () {
+  var offScreenCanvas = document.createElement('canvas')
+  return offScreenCanvas
+}
+
+export default imgTo64
